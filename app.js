cbcbc3dc0e8701bd5cbf37932904366e284b5d42
WaltSias <walt.sias@gmail.com>
2024-05-20 15:49:35 -0500

    updated

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 document.getElementById("fetch-user").addEventListener("click", function () {
-  fetch("https://randomuser.me/apii/") // Question 1: Why does this fetch call fail?
+  fetch("https://randomuser.me/api/") // Question 1: Why does this fetch call fail? # the url has an 2 "i" instead of 1 in /api/
     .then((data) => {
-      displayUser(data.results[0]); // Question 2: Why is data.results undefined?
+      displayUser(data.results[0]); // Question 2: Why is data.results undefined? ## Missing the " .then " to response to convert
     })
     .catch((error) => console.error("Fetch error:", error));
 });
 
 function displayUser(user) {
   const userInfoDiv = document.getElementById("user-info");
-  // Question 3: Why isn't the user's name displaying correctly?
-  userInfoDiv.innerHTML = `Name: ${user.first} ${user.name}<br>
+  // Question 3: Why isn't the user's name displaying correctly? ## Everything will be .first the object is name[]
+  userInfoDiv.innerHTML = `Name: ${user.name.first} ${user.name.last}<br>
                            Email: ${user.email}`;
 }
 
-// Question 4: Why does this API call fail?
+// Question 4: Why does this API call fail? ## the server and browser are not communicating properly .
 fetch("https://api.example.com/data")
   .then((response) => response.json())
   .catch((error) => {
@@ -26,10 +26,10 @@ function fetchNews() {
     .then((response) => response.json())
     .then((articles) => {
       const container = document.getElementById("news-container");
-      // Question 5: Why do the article titles not appear on the screen?
+      // Question 5: Why do the article titles not appear on the screen? ## p.textContent is for the name 
       articles.forEach((article) => {
         const p = document.createElement("p");
-        p.textContent = article.name;
+        p.textContent = article.title;
         container.appendChild(p);
       });
     })
